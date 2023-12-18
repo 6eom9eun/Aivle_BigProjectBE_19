8a3d107fdaed57d9251bf1d36361c2c5881bdd66
beomgeun <ggoon98@gmail.com>
2023-12-18 15:42:36 +0900

    authenticate, logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import signupView, CustomLoginView, profileView
+from .views import signupView, CustomLoginView, profileView, logoutView
 
 urlpatterns = [
     path('signup/', signupView, name='signup'),
     path('login/', CustomLoginView.as_view(), name='login'),
     path('profile/', profileView, name='profile'),
+    path('logout/', logoutView, name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,18 +1,29 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages 
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.views import LoginView
-from .forms import SignupForm, CustomLoginForm
+from .forms import UserCreationForm, CustomLoginForm
 
 def signupView(request): # 회원가입 뷰
     if request.method == 'POST':
-        form = SignupForm(request.POST)
+        form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user)
-            return redirect('signup')
+
+            authenticated_user = authenticate(
+                request, 
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password1']
+            )
+
+            if authenticated_user is not None:
+                login(request, authenticated_user)
+                messages.success(request, '가입 및 로그인 성공')
+                return redirect('profile')
+            else:
+                messages.error(request, '로그인에 실패했습니다.')
     else:
-        form = SignupForm()
+        form = UserCreationForm()
 
     return render(request, 'accounts/signup.html', {'form': form})
 
@@ -26,5 +37,10 @@ class CustomLoginView(LoginView): # 로그인 뷰
 
         return response
     
-def profileView(request): # 프로필 뷰
+def profileView(request):  # 프로필 뷰
     return render(request, 'accounts/profile.html', {'user': request.user})
+
+def logoutView(request): # 로그아웃 뷰
+    logout(request)
+    messages.success(request, '로그아웃 되었습니다.')
+    return redirect('login')
