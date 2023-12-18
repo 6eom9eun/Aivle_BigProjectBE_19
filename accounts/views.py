26a5016844acf833a86283acd61e9eba28dc1c70
beomgeun <ggoon98@gmail.com>
2023-12-18 15:55:08 +0900

    회원가입, 로그인 예외처리

--- accounts/views.py
+++ accounts/views.py
@@ -4,24 +4,33 @@ from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.views import LoginView
 from .forms import UserCreationForm, CustomLoginForm
 
+from django.contrib.auth.models import User
+
 def signupView(request): # 회원가입 뷰
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            user = form.save()
-
-            authenticated_user = authenticate(
-                request, 
-                username=form.cleaned_data['username'],
-                password=form.cleaned_data['password1']
-            )
-
-            if authenticated_user is not None:
-                login(request, authenticated_user)
-                messages.success(request, '가입 및 로그인 성공')
-                return redirect('profile')
+            username = form.cleaned_data['username']
+            
+            if User.objects.filter(username=username).exists(): # 아이디 존재 확인
+                messages.error(request, '이미 존재하는 사용자 ID 입니다.')
             else:
-                messages.error(request, '로그인에 실패했습니다.')
+                user = form.save()
+
+                authenticated_user = authenticate(
+                    request,
+                    username=username,
+                    password=form.cleaned_data['password1']
+                )
+
+                if authenticated_user is not None:
+                    login(request, authenticated_user)
+                    messages.success(request, '가입 및 로그인 성공')
+                    return redirect('profile')
+                else:
+                    messages.error(request, '로그인에 실패했습니다.')
+        else:
+            messages.error(request, '가입에 실패하였습니다. 올바른 정보를 입력해주세요.')
     else:
         form = UserCreationForm()
 
@@ -31,10 +40,13 @@ class CustomLoginView(LoginView): # 로그인 뷰
     form_class = CustomLoginForm
     template_name = 'accounts/login.html'
 
+    def form_invalid(self, form):
+        messages.error(self.request, '로그인에 실패했습니다. 올바른 정보를 입력해주세요.')
+        return super().form_invalid(form)
+
     def form_valid(self, form):
         response = super().form_valid(form)
         messages.success(self.request, '로그인 성공')
-
         return response
     
 def profileView(request):  # 프로필 뷰
