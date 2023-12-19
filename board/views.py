6a748aaff630ec6f1a0848a9f9cfe73464e70d66
hyunsu <cjswkans1224@gmail.com>
2023-12-19 10:45:40 +0900

    board list형태(수정해야함)

--- board/views.py
+++ board/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    context = {
+        'title': 'Board list',
+        'board_list': [
+            {'no':1, 'title': '목록1' },
+            {'no':2, 'title': '목록2' },
+            {'no':3, 'title': '목록3' },
+            {'no':4, 'title': '목록4' },
+            {'no':5, 'title': '목록5' }
+        ]
+    }
+    return render(request, 'board/index.html', context)
