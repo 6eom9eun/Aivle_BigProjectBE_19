e4de8dc4b8bc6d6eab05dbb5c37e83d90e191a3c
beomgeun <ggoon98@gmail.com>
2023-12-19 10:28:02 +0900

    post 모델

--- accounts/views.py
+++ accounts/views.py
@@ -4,10 +4,12 @@ from django.contrib.auth.models import User
 
 from .serializers import SignupSerializer, LoginSerializer
 
+# 회원가입 뷰 : 생성 기능 -> CreateAPIView
 class SignupView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = SignupSerializer
 
+# 로그인 뷰 : 모델 영향 X -> GenericAPIView 상속
 class LoginView(generics.GenericAPIView):
     serializer_class = LoginSerializer
     def post(self, request):
--- board/models.py
+++ board/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+\
+class Post(models.Model):
+    post_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    published_at = models.DateTimeField(null=True, blank=True)
 
-# Create your models here.
+    def publish(self):
+        self.published_at = timezone.now()
+        self.save()
