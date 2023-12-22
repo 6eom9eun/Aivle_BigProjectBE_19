0169dd75199c16dd79ac60e72de43b0afafc0166
beomgeun <ggoon98@gmail.com>
2023-12-22 13:54:26 +0900

    문제 저장

--- study/models.py
+++ study/models.py
@@ -7,7 +7,12 @@ class Word(models.Model):
 
     def __str__(self):
         return f"{self.word} - {self.meaning}"
-
+    
+class Quiz(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    word = models.ForeignKey(Word, on_delete=models.CASCADE)
+    is_correct = models.BooleanField(default=False)
+    solved_date = models.DateTimeField(auto_now_add=True)
 
 class Audio(models.Model):
     audio_id = models.AutoField(primary_key=True)
--- study/views.py
+++ study/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Word
+from .models import Word, Quiz
 from .gpt import *
 from django.http import JsonResponse
 
@@ -11,20 +11,22 @@ from rest_framework.authentication import TokenAuthentication
 class RandomQuizView(APIView):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
-    
+
     def get(self, request):
-        # 데이터베이스에서 무작위 레코드 단어와 뜻을 가져옴
         random_word_entry = Word.objects.order_by('?').first()
-        print(random_word_entry.word)
+
         if random_word_entry:
-            # 랜덤 항목에서 단어와 뜻 추출
             word = random_word_entry.word
             meaning = random_word_entry.meaning
 
-            # 질문 생성
             response = make_problem(word, meaning)
 
-            # 사용자에게 응답 반환
-            return JsonResponse({"question_response": response}, status=status.HTTP_200_OK)
+            if request.user.is_authenticated:
+                quiz_instance = Quiz(user=request.user, word=random_word_entry)
+                quiz_instance.save()
+
+                return JsonResponse({"question_response": response}, status=status.HTTP_200_OK)
+            else:
+                return JsonResponse({"error": "인증되지 않은 사용자입니다."}, status=status.HTTP_401_UNAUTHORIZED)
         else:
             return JsonResponse({"error": "데이터베이스에서 단어를 찾을 수 없습니다."}, status=status.HTTP_404_NOT_FOUND)
