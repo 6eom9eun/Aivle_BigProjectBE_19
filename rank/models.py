c0613545b592ef18cb6668ad071fae121ba56f5e
beomgeun <ggoon98@gmail.com>
2023-12-22 13:33:35 +0900

    기본값 1

--- rank/models.py
+++ rank/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 class Ranking(models.Model):
     lv_id = models.AutoField(primary_key=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='ranking') 
-    user_level = models.IntegerField() #단어장 레벨
+    user_level = models.IntegerField(default=1) #단어장 레벨
     answers = models.IntegerField(default=0)  #푼 문제수
     created_dt=models.DateTimeField(null=True, blank = True)       #수정날짜 표시(등수 같을 경우 순위 판단)
     
