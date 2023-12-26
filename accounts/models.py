03713fa127bc825008ac1cc60787b38b787d7688
beomgeun <ggoon98@gmail.com>
2023-12-26 15:26:04 +0900

    퀴즈

--- accounts/models.py
+++ accounts/models.py
@@ -2,14 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from rank.models import Ranking
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     introduction = models.TextField(default="안녕하세요", blank=True, null=True)  # 자기소개
     image = models.ImageField(upload_to='profile/', default='default_profile.png')  # 프로필 사진
+
     def __str__(self):
         return self.user.username
-
+ 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
