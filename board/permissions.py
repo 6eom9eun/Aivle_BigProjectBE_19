5ba5b48a4be81aa6ea7c27ed70f807cb3a166bce
hyunsu <cjswkans1224@gmail.com>
2023-12-26 18:01:56 +0900

    Board Create, Write 수정

--- board/permissions.py
+++ board/permissions.py
@@ -1,5 +1,16 @@
 from rest_framework import permissions
 
+## 게시글 ==> 조회는 누구나 가능 / 작성은 로그인한 유저만 / 수정, 삭제는 작성자만
+class IsOwnerOrReadOnly(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        # 읽기 권한 요청이 들어오면 허용
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        # 요청자(request.user)가 객체(Board)의 user와 동일한지 확인
+        return obj.user == request.user
+
 class CustomReadOnly(permissions.BasePermission):
     ## 게시글 ==> 조회는 누구나 가능 / 작성은 로그인한 유저만 / 수정은 작성자만
     def has_permission(self, request, view):
@@ -10,11 +21,11 @@ class CustomReadOnly(permissions.BasePermission):
         return False
     
     # 각 객체별 권한
-    # SAFE_METHOD(GET, HEAD, OPTIONS)로 요청이 들어온 경우에는 method를 허용을 해주고,
-    # 그 외(PUT, PATCH, DELETE)에는 게시글의 user와 로그인된 user가 동일한 경우에만 권한을 허용
     def has_object_permission(self, request, view, obj):
-        # 데이터에 영향을 미치지 않는 메소드 (GET 등)은 통과
+        # 안전한 메서드(GET, HEAD, OPTIONS)로 요청이 들어오면 허용
         if request.method in permissions.SAFE_METHODS:
             return True
+        
         # PUT/PATCH 등의 경우에는 요청으로 들어온 유저와 객체의 유저를 비교해 같으면 통과
-        return obj.author == request.user
+        # 요청자(request.user)가 객체(Post)의 user와 동일한지 확인
+        return obj.user == request.user
--- board/urls.py
+++ board/urls.py
@@ -2,25 +2,11 @@
 from django.urls import path, include
 
 from . import views
-from .views import PostViewSet, CommentViewSet
-from rest_framework.routers import DefaultRouter
-
-router = DefaultRouter()
-router.register('', PostViewSet, basename='post') # (게시글)
-# router.register('', CommentViewSet, basename='comment') # (댓글)
+from .views import PostViewSet, PostDetailViewSet, CommentViewSet
 
 app_name = 'board'
 urlpatterns = [
-    path('', include(router.urls)),
-    path("<int:post_pk>/comments/", CommentViewSet.as_view({"post": "create", "get": "list"}), name="post-detail"),
+    path('', PostViewSet.as_view(), name = "post-list"),
+    path('<int:post_id>/', PostDetailViewSet.as_view(), name="post-detail"),
+    path("<int:post_id>/comments/", CommentViewSet.as_view({"post": "create", "get": "list"}), name="comment-detail"),
 ]
-
-"""
-    from rest_framework import routers
-    router = routers.SimpleRouter()
-
-    from .views import PostViewSet
-
-    router.register('posts', PostViewSet)
-    
-"""
