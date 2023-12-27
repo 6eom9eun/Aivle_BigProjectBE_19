2a4b99117e3254c8b350424fa70779e0edffa527
beomgeun <ggoon98@gmail.com>
2023-12-27 14:38:27 +0900

    머지

--- board/views.py
+++ board/views.py
@@ -9,52 +9,39 @@ from rest_framework import generics
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from rest_framework.exceptions import PermissionDenied
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from django.shortcuts import get_object_or_404, get_list_or_404
 
-# Post의 목록 보여주기
+# Post의 목록, detail 보여주기, 수정하기, 삭제하기 모두 가능
 class PostViewSet(generics.ListCreateAPIView):
     queryset = Post.objects.all().order_by('-created_at') # 생성일자기준으로 내림차순
     serializer_class = PostSerializer
     authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly] # 비인증 요청에 대해서는 읽기만 허용
+    permission_classes = [IsAuthenticatedOrReadOnly, CustomReadOnly] # 비인증 요청에 대해서는 읽기만 허용
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['user', 'published_at']
     
     def perform_create(self, serializer):
         serializer.save(user = self.request.user)
 
-# Post 조회, 수정, 삭제
 class PostDetailViewSet(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all().order_by('-created_at') # 생성일자기준으로 내림차순
     serializer_class = PostDetailSerializer
     authentication_classes = [TokenAuthentication]
-    permission_classes = [IsOwnerOrReadOnly] # 비인증 요청에 대해서는 읽기만 허용
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly] # 비인증 요청에 대해서는 읽기만 허용
     lookup_field = 'post_id'
-    # partial_update = True  # 부분 업데이트 허용 설정
+    partial_update = True  # 부분 업데이트 허용 설정
         
-# (댓글) Comment 목록 보여주기
-class CommentViewSet(generics.ListCreateAPIView):
+# (댓글) Comment 보여주기, 수정하기, 삭제하기 모두 가능
+class CommentViewSet(viewsets.ModelViewSet):
     authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    permission_classes = [IsAuthenticatedOrReadOnly, CustomReadOnly]
     serializer_class = CommentSerializer
-    lookup_url_kwarg = 'post_id'
     
     def get_queryset(self):
         post_pk = self.kwargs.get("post_id")
         return Comment.objects.filter(reply__post_id=post_pk).order_by('-created_at')
 
     def perform_create(self, serializer):
-        post_pk = self.kwargs.get("post_id")
-        post = get_object_or_404(Post, pk=post_pk)
-        serializer.save(user=self.request.user, reply=post)
-        
-# (댓글) Comment 조회, 수정, 삭제     
-class CommentDetailView(generics.RetrieveUpdateDestroyAPIView):
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticatedOrReadOnly]
-    serializer_class = CommentSerializer
-    lookup_url_kwarg = 'comment_id'
-    queryset = Comment.objects.all()
+        serializer.save(user=self.request.user, reply_id=self.kwargs.get("post_id"))
