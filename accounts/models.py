abc01fac3577d91b26c0f68b1b5b320c705859af
beomgeun <ggoon98@gmail.com>
2023-12-27 11:45:52 +0900

    mbti 주석처리

--- accounts/models.py
+++ accounts/models.py
@@ -5,9 +5,29 @@ from django.dispatch import receiver
 from rank.models import Ranking
 
 class Profile(models.Model):
+    # MBTI = [
+    # ('mbti_istj', 'ISTJ'),
+    # ('mbti_istp', 'ISTP'),
+    # ('mbti_isfj', 'ISFJ'),
+    # ('mbti_isfp', 'ISFP'),
+    # ('mbti_intj', 'INTJ'),
+    # ('mbti_intp', 'INTP'),
+    # ('mbti_infj', 'INFJ'),
+    # ('mbti_infp', 'INFP'),
+    # ('mbti_estj', 'ESTJ'),
+    # ('mbti_estp', 'ESTP'),
+    # ('mbti_esfj', 'ESFJ'),
+    # ('mbti_esfp', 'ESFP'),
+    # ('mbti_entj', 'ENTJ'),
+    # ('mbti_entp', 'ENTP'),
+    # ('mbti_enfj', 'ENFJ'),
+    # ('mbti_enfp', 'ENFP'),
+    # ('mbti_none', '선택안함'),
+    # ]
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     introduction = models.TextField(default="안녕하세요", blank=True, null=True)  # 자기소개
     image = models.ImageField(upload_to='profile/', default='default_profile.png')  # 프로필 사진
+    # mbti = models.CharField(max_length=30, choices=MBTI, default="mbti_none")
 
     def __str__(self):
         return self.user.username
