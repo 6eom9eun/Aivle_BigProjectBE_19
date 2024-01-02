29075dfa433887a3c42bcc786742f03b50db3e11
beomgeun <ggoon98@gmail.com>
2024-01-02 21:01:39 +0900

    댓글에 프로필 이미지 보여주기

--- board/serializers.py
+++ board/serializers.py
@@ -4,13 +4,13 @@ from .models import Post, Comment
           
 class CommentSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source='user.username')
-    image = serializers.ReadOnlyField(source='profile.image')
+    image = serializers.ReadOnlyField(source='profile.image.url') # 이미지가 경로 url 이면 .url 붙여야 됨.
 
     class Meta:
         model = Comment
         fields = ['comment_id', 'user', 'image', 'reply', 'created_at', 'comment']
-        read_only_fields = ['reply'] 
-
+        read_only_fields = ['reply']
+        
 class PostSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source = 'user.username') # views.py에서 넘겨준 user의 username 값 받아옴
     class Meta:
