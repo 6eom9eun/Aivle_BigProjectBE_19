131e06d5c9000eede2791bfc61476271966d0a5f
beomgeun <ggoon98@gmail.com>
2024-01-02 16:26:36 +0900

    작문 Api

--- study/urls.py
+++ study/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', QuizListView.as_view(), name='quiz-list'),
     path('quiz/', RandomQuizView.as_view(), name='quiz-random'),
     path('quiz/<int:quiz_id>/', QuizDetailView.as_view(), name='quiz-detail'),
+    path('quiz/<int:quiz_id>/sentence/', MakeSentenceView.as_view(), name='quiz-sentence'),
     path('quiz/<int:quiz_id>/ocr/', OcrView.as_view(), name='quiz-ocr'),
     path('quiz/<int:quiz_id>/stt/', SpeechToTextView.as_view(), name='quiz-stt'),
     path('quiz/<int:quiz_id>/tts/', TextToSpeechView.as_view(), name='quiz-tts'),
