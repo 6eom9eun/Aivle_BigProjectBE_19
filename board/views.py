a867c041c1a2c5d55ee4aa1ba49949476fb8cb08
beomgeun <ggoon98@gmail.com>
2024-01-05 09:34:13 +0900

    업로드 이미지 삭제 api

--- board/views.py
+++ board/views.py
@@ -9,6 +9,7 @@ from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.views import APIView
 
@@ -75,13 +76,28 @@ class CommentDetailView(generics.RetrieveUpdateDestroyAPIView):
     
 # 이미지 업로드 뷰
 class ImageUploadView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    
     def post(self, request, *args, **kwargs):
         if request.method == 'POST':
             image_file = request.FILES.get('image')
             if image_file:
                 new_image = Image.objects.create(image=image_file)
                 image_url = new_image.image.url
-                response_data = {'image_url': image_url}
+                response_data = {
+                    'image_id': new_image.id,
+                    'image_url': image_url
+                }
                 return JsonResponse(response_data, status=status.HTTP_201_CREATED)
             else:
-                return JsonResponse({'error': 'No image file'}, status=status.HTTP_400_BAD_REQUEST)
+                return JsonResponse({'error': '이미지 파일이 없습니다.'}, status=status.HTTP_400_BAD_REQUEST)
+            
+    def delete(self, request, image_id, *args, **kwargs):
+        try:
+            image_instance = Image.objects.get(pk=image_id)
+        except Image.DoesNotExist:
+            return JsonResponse({'error': '이미지를 찾을 수 없습니다.'}, status=status.HTTP_404_NOT_FOUND)
+        
+        image_instance.delete()
+        return JsonResponse({'message': '성공적으로 삭제 완료 !'}, status=status.HTTP_204_NO_CONTENT)
