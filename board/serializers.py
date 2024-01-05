db383a39f655633d1c3fc93edfc174a45741cdbc
beomgeun <ggoon98@gmail.com>
2024-01-05 13:49:12 +0900

    'user_id', 'username'

--- board/serializers.py
+++ board/serializers.py
@@ -2,28 +2,31 @@ from rest_framework import serializers
 from .models import Post, Comment, Image
 
 class CommentSerializer(serializers.ModelSerializer):
-    user = serializers.ReadOnlyField(source='user.username')
+    user_id = serializers.ReadOnlyField(source='user.id')
+    username = serializers.ReadOnlyField(source='user.username')
     image = serializers.ReadOnlyField(source='profile.image.url') # 이미지가 경로 url 이면 .url 붙여야 됨.
 
     class Meta:
         model = Comment
-        fields = ['comment_id', 'user', 'image', 'reply', 'created_at', 'comment']
+        fields = ['comment_id', 'user_id', 'username', 'image', 'reply', 'created_at', 'comment']
         read_only_fields = ['reply']
         
 class PostSerializer(serializers.ModelSerializer):
-    user = serializers.ReadOnlyField(source = 'user.username') # views.py에서 넘겨준 user의 username 값 받아옴
+    user_id = serializers.ReadOnlyField(source = 'user.id') # views.py에서 넘겨준 user의 username 값 받아옴
+    username = serializers.ReadOnlyField(source='user.username')
     class Meta:
         model = Post
-        fields = ['post_id', 'user', 'title', 'created_at', 'image']
+        fields = ['post_id', 'user_id', 'username', 'title', 'created_at', 'image']
 
 class PostDetailSerializer(serializers.ModelSerializer):
-    user = serializers.ReadOnlyField(source='user.username')
+    user_id = serializers.ReadOnlyField(source='user.id')
+    username = serializers.ReadOnlyField(source='user.username')
     comments = CommentSerializer(many=True)
     image = serializers.ImageField(use_url=True)
 
     class Meta:
         model = Post
-        fields = ['post_id', 'user', 'title', 'content', 'created_at', 'comments', 'image']
+        fields = ['post_id', 'user_id', 'username', 'title', 'content', 'created_at', 'comments', 'image']
 
 class PostCreateSerializer(serializers.ModelSerializer):
     class Meta:
