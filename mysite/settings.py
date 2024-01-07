ea5fc1e457ce5815d5bb323ff61fb3f7c1c97a2a
hyunsu <cjswkans1224@gmail.com>
2024-01-07 13:11:13 +0900

    kakao login 수정

--- mysite/settings.py
+++ mysite/settings.py
@@ -128,7 +128,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'allauth.account.middleware.AccountMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
