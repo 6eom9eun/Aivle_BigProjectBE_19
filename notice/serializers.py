bc611162d361b654aff9053d384a61d42c228820
beomgeun <ggoon98@gmail.com>
2024-01-10 14:02:38 +0900

    notice list

--- notice/serializers.py
+++ notice/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
 from .models import Notice
 
+class NoticeListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Notice
+        fields = ['id', 'title', 'created_at']
+        
 class NoticeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Notice
--- notice/views.py
+++ notice/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
 from .models import Notice
-from .serializers import NoticeSerializer
+from .serializers import NoticeListSerializer, NoticeSerializer
 from rest_framework.authentication import TokenAuthentication
 from .permission import AdminPermission
 
@@ -8,7 +8,7 @@ class NoticeListView(generics.ListCreateAPIView):
     authentication_classes = [TokenAuthentication]
     permission_classes = [AdminPermission]
     queryset = Notice.objects.all()
-    serializer_class = NoticeSerializer
+    serializer_class = NoticeListSerializer
     
 class NoticeDetailView(generics.RetrieveUpdateDestroyAPIView):
     authentication_classes = [TokenAuthentication]
