aa0037add8462969e3efe673a5f3b56fa2bc92c8
beomgeun <ggoon98@gmail.com>
2024-01-10 13:50:26 +0900

    공지사항

--- notice/urls.py
+++ notice/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from .views import NoticeListView, NoticeDetailView
 
+app_name = 'notice'
 urlpatterns = [
-    path('', NoticeListView.as_view(), name='notice-list-create'),
+    path('', NoticeListView.as_view(), name='notice-list'),
     path('<int:pk>/', NoticeDetailView.as_view(), name='notice-detail'),
 ]
--- notice/views.py
+++ notice/views.py
@@ -1,18 +1,17 @@
 from rest_framework import generics
 from .models import Notice
-from rest_framework import permissions
 from .serializers import NoticeSerializer
-
-class AdminPermission(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return request.user and request.user.is_authenticated and request.user.is_staff
+from rest_framework.authentication import TokenAuthentication
+from .permission import AdminPermission
 
 class NoticeListView(generics.ListCreateAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [AdminPermission]
     queryset = Notice.objects.all()
     serializer_class = NoticeSerializer
-    permission_classes = [AdminPermission]
-
+    
 class NoticeDetailView(generics.RetrieveUpdateDestroyAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [AdminPermission]
     queryset = Notice.objects.all()
     serializer_class = NoticeSerializer
-    permission_classes = [AdminPermission]
