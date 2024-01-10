041ac8425805a84f510e1b377b2514c038ccfe78
beomgeun <ggoon98@gmail.com>
2024-01-10 15:34:49 +0900

    order_by

--- notice/views.py
+++ notice/views.py
@@ -3,21 +3,17 @@ from .models import Notice
 from .serializers import NoticeSerializer, NoticeListSerializer, NoticeCreateSerializer
 from rest_framework.authentication import TokenAuthentication
 from .permission import AdminPermission
+from .models import Notice
 
 class NoticeListView(generics.ListCreateAPIView):
     authentication_classes = [TokenAuthentication]
     permission_classes = [AdminPermission]
-    queryset = Notice.objects.all()
-    
-    def get_queryset(self):
-        return Notice.objects.all().order_by('-id')
+    queryset = Notice.objects.order_by('-id')
     
     def get_serializer_class(self):
         if self.request.method == 'POST' or self.request.method == 'PUT':
             return NoticeCreateSerializer
         return NoticeListSerializer
-
-    ordering = ['-id']
     
 class NoticeDetailView(generics.RetrieveUpdateDestroyAPIView):
     authentication_classes = [TokenAuthentication]
