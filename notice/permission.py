3f8867c1365262c2e39789af253de059b2f0db09
beomgeun <ggoon98@gmail.com>
2024-01-10 14:17:55 +0900

    권한 에러 메세지 커스텀

--- notice/permission.py
+++ notice/permission.py
@@ -1,7 +1,13 @@
 from rest_framework import permissions
+from rest_framework.exceptions import PermissionDenied
 
 class AdminPermission(permissions.BasePermission):
+    message = '관리자만 접근할 수 있습니다.'
+
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
         return request.user and request.user.is_authenticated and request.user.is_staff
+
+    def has_object_permission(self, request, view, obj):
+        return request.method in permissions.SAFE_METHODS or (request.user and request.user.is_authenticated and request.user.is_staff)
